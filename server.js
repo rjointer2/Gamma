8d631118aa2f1e6d6c6da9a2d67b60f0469ea03d
Senait <senutekie77@gmail.com>
2021-06-27 10:51:46 -0700

    database connected

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-//const { MONGODB } = require('./config');
+const { MONGODB } = require('./config');
 const typeDefs = gql`
     type Query {
         sayHi: String!
@@ -21,11 +21,11 @@ const server = new ApolloServer({
     resolvers
 })
 
-// mongoose.connect(MONGODB, {useNewUrlParser: true})
-// .then(() => {
-//     console.log('MongoDB connected!!')
-//     return server.listen({ port: 5000})
-// })
-// .then(res => {
-//     console.log(`Server running at ${res.url}`)
-// })
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+.then(() => {
+    console.log('MongoDB connected!!')
+    return server.listen({ port: 5000})
+})
+.then(res => {
+    console.log(`Server running at ${res.url}`)
+})
