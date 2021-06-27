a2a24b6ac183fbb91342e1ba2aa9505ffa079cad
Senait <senutekie77@gmail.com>
2021-06-27 10:49:26 -0700

    npm packages installed for the server and GraphQL defined

---
+++ server.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+//const mongoose = require('mongoose');
+
+//const { MONGODB } = require('./config');
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'HelloWorld!'
+    }
+}
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+// mongoose.connect(MONGODB, {useNewUrlParser: true})
+// .then(() => {
+//     console.log('MongoDB connected!!')
+//     return server.listen({ port: 5000})
+// })
+// .then(res => {
+//     console.log(`Server running at ${res.url}`)
+// })
