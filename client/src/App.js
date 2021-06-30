20cfcc1af8d768716df21b5b1baa107230e278cc
Roody <rjointer2@gmail.com>
2021-06-29 21:23:34 -0700

    dev made and task out

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Components
+
+import Navbar from "./components/Navbar/Navbar";
+import Game from './components/Game/Game';
+
+// Pages
+
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Navbar}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/signup" component={SignUp}/>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/game" component={Game}/>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+
+
