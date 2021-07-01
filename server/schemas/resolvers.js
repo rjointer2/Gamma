ade7a73104a12b57e482b1ec001cfaea611bff6e
Senait <senutekie77@gmail.com>
2021-06-30 21:16:41 -0700

    mutation for add user and login added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,5 +32,35 @@ const resolvers = {
           .populate('comments');
       },
       
-    }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = singleToken(user);
+
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            // checks if the password is correct
+            const correctPw = await user.isCorrectPassword(password);
+            
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user }
+        },
+
+   }
 }
+
+module.exports = resolvers;
