84909b1f8624e46e5d9129822087eace4732c640
Senait <senutekie77@gmail.com>
2021-06-30 21:02:32 -0700

    get all users in resolvers sub directory

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password')
+                .populate('')
+                .populate('');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('You are not logged in!');
+        },
+        
+         // get all users
+    users: async () => {
+        return User.find()
+          .select('-__v -password')
+          .populate('')
+          .populate('');
+      },
+      
+    }
+}
