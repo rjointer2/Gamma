3a8aee9431b5b9dad5f26c7e2259ea294cf71f92
Senait <senutekie77@gmail.com>
2021-06-30 19:59:35 -0700

    queries and mutations defined for the user login in the typeDefs sub directory

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+// setting the type definition/ schemas
+const typeDefs = gql`
+  
+  type User {
+      _id: ID
+      username: String
+      email: String
+
+      type Query {
+          me: User
+          users: [User]
+          user(username: String!):User
+      }
+
+      type Auth {
+          token: ID
+          user: User
+      }
+
+      type Mutation {
+          login(email: String!, password: String!): Auth
+          addUser(Username: String!, email: String!, password: String!): Auth
+      }
+
+  }
+
+`;
+
+module.exports = typeDefs;
