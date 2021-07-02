8d8c309abee1aef3a589f568b603084212ecf153
Senait <senutekie77@gmail.com>
2021-07-01 21:28:33 -0700

    usestate useEffect and useRef set up in the chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,7 +7,15 @@ import io from "socket.io-client";
 
 const Chat = () => {
   
-    
+    const [yourID, setYourID] = useState();
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState("");
+
+    const socketRef = useRef();
+
+    useEffect(() => {
+        
+    });
     return (
         <div>
          <Page></Page>
