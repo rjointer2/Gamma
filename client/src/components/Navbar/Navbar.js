c60c6e403505e0cb7a1b1dbe8d2421f4da7b7ddc
Hung Tang <hungtang@hotmail.com>
2021-07-01 21:33:07 -0700

    baselined navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,48 +1,36 @@
-// import './NavbarStyles.js';
-import { useEffect } from 'react';
-// import { capitalizeFirstLetter } from '../../utils/helpers';
-
-const Navbar = (props) => {
-    const {
-        categories = [],
-        setCurrentCategory,
-        currentCategory,
-        contactSelected,
-        setContactSelected
-    } = props;
-
-    useEffect(() => {
-        // document.title = capitalizeFirstLetter(currentCategory.name);
-        document.title = currentCategory.name;
-    }, [currentCategory]);
-    return (
-        <div class="navbar">
-            <div class="navbar-title">
-                <h1>Gamma</h1>
-            </div>
-            <div class="navbar-list">
-                <nav>
-                    <ul className="flex-row">
-                        {/* <li className="mx-2">
-                            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>About me</a>
-                        </li> */}
-                        {categories.map((category) => (
-                            <li className={`mx-1 ${
-                                currentCategory.name === category.name && !contactSelected && `navActive`
-                                }`} key={category.name}>
-                                <span onClick={() => { 
-                                    setCurrentCategory(category);
-                                    setContactSelected(false);
-                                    }}>
-                                    {category.name}
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    );
-}
+// importing styled components styling for navbar
+import {
+    Nav,
+    NavLink,
+    NavTitle,
+    NavMenu
+} from './NavbarStyles';
+    
+// Navbar component
+const Navbar = () => {
+return (
+    <>
+    <Nav>
+        <NavTitle to='/Sign-in' activeStyle>
+            Gamma
+        </NavTitle>
+        <NavMenu>
+            <NavLink to='/Favorite' activeStyle>
+            Favorite
+            </NavLink>
+            <NavLink to='/Invite' activeStyle>
+            Invite
+            </NavLink>
+            <NavLink to='/Games' activeStyle>
+            Games
+            </NavLink>
+            <NavLink to='/SignOut' activeStyle>
+            Sign Out
+            </NavLink>
+        </NavMenu>
+    </Nav>
+    </>
+);
+};
 
 export default Navbar;
