6c75d04f79bb21311fb82a555c6b3b9584e50117
Roody <rjointer2@gmail.com>
2021-07-07 11:22:07 -0700

    cleaned up home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,21 +1,11 @@
 
-import Modal from '../../components/Modal/Modal';
-import Navbar from '../../components/Navbar/Navbar'
-import { Container } from '../../globalStyles/aligment';
-import { Text } from '../../globalStyles/text';
 
 const Home = () => {
 
     return (
         <div>
-            <Navbar />
-            { true && <Modal/> }
-            <Container>
-                <div>
-                    hi
-                </div>
-            </Container>
-        </div>
+            
+        </div>  
     )
 }
 
