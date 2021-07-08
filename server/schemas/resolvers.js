be8466ebcd980d04ad8d5ce9b2bad7528c55fe55
Roody <rjointer2@gmail.com>
2021-07-08 02:16:36 -0700

    time to test the addFriendsToUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,20 +16,26 @@ let resolvers = {
         },
         
         // get all users
-            users: async () => {
-                return User.find()
-                .select('-__v -password')
-                .populate('Friend')
-                .populate('Chat');
-            },
-
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('Friend')
+            .populate('Chat');
         },
+
+    },
 
     Mutation: {
         addUser: async (parent, args) => {
             try {
-                const user = await User.create(args);
-                console.log(user)
+                const user = await User.create({
+                    username: args.username,
+                    friends: "[]",
+                    email: args.email,
+                    password: args.password,
+                });
+                console.log(user);
+                console.log(args)
                 const token = signToken(user);
 
                 if(!token) {
@@ -58,7 +64,28 @@ let resolvers = {
             const token = signToken(user);
             return { token, user }
         },
+        addFriendToUser: async (parent, {username, friendUsername}) => {
 
+            if( !username || !friendUsername ) throw new Error("Field or fields are filled");
+            // not efficient but okay for now
+            const user = await User.findOne({ username });
+            const friend = await User.findOne({ friendUsername });
+            // if friend doesn't exist
+            if(!user || !friend) throw new Error(`Entry or entries don't exist`);
+            // add friend in the friend array
+            const friendArray = JSON.parse(user.friends);
+            friendArray.push(friendUsername);
+            const stringedFriendArray = JSON.stringify(friendArray);
+            user.friends = stringedFriendArray;
+            // save the document
+            await user.save();
+            
+            return {
+                username: username,
+                friends: user.friends
+            }
+            
+        }
    }
 }
 
