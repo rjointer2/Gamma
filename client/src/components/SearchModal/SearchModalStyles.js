0b58c9711e626ce099b67945cd40a699b3f93a9f
Hung Tang <hungtang@hotmail.com>
2021-07-07 22:58:52 -0700

    made tweaks to modal

--- client/src/components/SearchModal/SearchModalStyles.js
+++ client/src/components/SearchModal/SearchModalStyles.js
@@ -7,16 +7,21 @@ export const ModalBackground = styled.div`
     display: flex; 
     position: fixed; 
     z-index: 20; 
-    top: 0;
-    transform: translate(-70%);
+    // top: 0;
+    // transform: translate(-70%);
+    position: absolute;
+    top: 35%;
+    left: 66%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)
     // overflow: auto; 
     // background-color: rgb(0,0,0); 
     // background-color: rgba(0,0,0,0.4); 
 `;
 
 export const ModalWrapper = styled.div`
-    width: 500px;
-    height: 500px;
+    width: 350px;
+    height: 250px;
     box-shadow: 0 5px 16px rgba(0,0.0,0.2);
     background: #fff;
     color: #000;
@@ -36,9 +41,17 @@ export const ModalContent = styled.div`
 `;
 
 //  styling
-export const ModalClose = styled.span`
+export const ModalClose = styled.div`
     color: #aaa;
     // float: right;
     font-size: 28px;
     font-weight: bold;
+    cursor: pointer;
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    padding: 0 8px;
+    // transform: translate(0%, -50%);
+    border: 2px solid purple;
+    border-radius: 5px;
 `;
