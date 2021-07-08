c9bdb72d23abbc3b8574e49a3287b0ac461b7be6
Roody <rjointer2@gmail.com>
2021-07-08 10:04:30 -0700

    auth is working but I have to store the token in local storage now!

--- client/src/ulit/auth/authClient.js
+++ client/src/ulit/auth/authClient.js
@@ -5,12 +5,43 @@ import decode from 'jwt-decode';
 // create a new class to instantiate for a user
 class AuthClient {
 
-    login(idToken) {
-        // Saves user token to localStorage
-        localStorage.setItem('id_token', idToken);
-        // go to home page
-        window.location.assign('/home');
-      }
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token_gamma', idToken);
+    // go to home page
+    window.location.assign('/home');
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token_gamma');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
 }
 
 export default new AuthClient()
