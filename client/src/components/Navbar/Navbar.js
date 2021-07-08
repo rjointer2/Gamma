b41094994b973dc280316be696cf2008fc4d480b
Muh <mmtariqk@gmail.com>
2021-07-08 14:00:55 -0700

    reactjs nav

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,39 +1,65 @@
-// importing styled components styling for navbar
-import {
-    Nav,
-    NavLink,
-    NavTitle,
-    NavMenu
-} from './NavbarStyles';
-    
-// Navbar component
-const Navbar = () => {
-return (
-    <>
-    <Nav>
-        <NavTitle to='/Sign-in' activeStyle>
-            Gamma
-        </NavTitle>
-        <NavMenu>
-            <NavLink to='/Favorite' activeStyle>
-            Favorite
-            </NavLink>
-            <NavLink to='/Favorite' activeStyle>
-            Chats
-            </NavLink>
-            <NavLink to='/Invite' activeStyle>
-            Invite
-            </NavLink>
-            <NavLink to='/Games' activeStyle>
-            Games
-            </NavLink>
-            <NavLink to='/Sign-in' activeStyle>
-            Sign Out
-            </NavLink>
-        </NavMenu>
-    </Nav>
-    </>
-);
-};
+// Styles
+import './Navbar.css';
+
+// Array of links
+
+import { Link } from 'react-router-dom';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const Navbar = ({click}) => {
+    return (
+        <nav className="navbar">
+            {/* logo */}
+            <div className="navbar_logo">
+                <h2>
+                    Employee Managment System
+                </h2>
+            </div>
+            {/* Links */}
+            {/* 
+            
+                map out the the links and with jsx dynmaically map
+                each object's prop in the to attr
+            */}
+
+                <ul className="navbar_links">
+
+                { SidebarData.map((val, link) => {
+                    return (
+                            <li>
+                            <Link to={val.link} className='nav_link'>
+                                {val.icon}
+                            </Link>
+                        </li>
+                        )  
+                    }) 
+                }
+
+                </ul>
+                
+
+            {/* 
+                Once the state is true
+            */}
+            <div className="hamburger_menu" onClick={click}>
+               <MenuIcon />
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar;
+
+/*
+<ul className="navbar_links">
+                    { SidebarData.map((val, link) => {
+                        <li>
+                            <Link to={val.link} className='nav_link'>
+                                {val.icon}
+                            </Link>
+                        </li>
+                    }) }
+                    </ul>
+*/
